3d709e87c7fe522b70f9d537adc8a732cce46d44
yazeedb <ybzadough@gmail.com>
2018-05-16 19:57:36 -0400

    connect to GraphQL

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import Link from './Link';
 
-const linksToRender = [
-  {
-    id: '1',
-    description: 'Prisma turns your database into a GraphQL API 😎 😎',
-    url: 'https://www.prismagraphql.com',
-  },
-  {
-    id: '2',
-    description: 'The best GraphQL client',
-    url: 'https://www.apollographql.com/docs/react/',
-  },
-];
-
-const LinkList = () => (
+const LinkList = ({
+  feedQuery: { error, feed, loading }
+}) => (
   <div>
+    { console.log(error, feed, loading) }
+    { loading && <div>Loading...</div> }
+    { error && <div>Error { error }</div> }
     {
-      linksToRender
-        .map((link) => <Link key={ link.id } link={ link } />)
+      feed && feed.links.map((link) => (
+        <Link key={ link.id } link={ link } />
+      ))
     }
   </div>
 );
 
-export default LinkList;
+const FEED_QUERY = gql`
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`;
+
+export default graphql(FEED_QUERY, { name: 'feedQuery' }) (LinkList);
