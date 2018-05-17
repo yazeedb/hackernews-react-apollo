38e807f395988968175075d5d91fa46267706b49
yazeedb <ybzadough@gmail.com>
2018-05-16 20:35:22 -0400

    add Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
 import '../styles/App.css';
 
 const App = () => (
-  <CreateLink />
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Switch>
+        <Route exact path="/" component={LinkList} />
+        <Route exact path="/create" component={CreateLink} />
+      </Switch>
+    </div>
+  </div>
 );
 
 export default App;
